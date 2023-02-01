fb748dfa12842e8c5ebbefcfdb3d592966b541b1
ACarmonaTS <ts.acarmona@outlook.com>
2023-01-31 18:31:41 -0600

    Iniciando header

---
+++ src/components/site/app.js
@@ -0,0 +1,7 @@
+import header from "./header/header.js"
+
+function App() {
+  return header();
+}
+
+export default App;
---
+++ src/components/site/header/header.js
@@ -0,0 +1,5 @@
+function header(){
+  return <div>Hola</div>
+}
+
+export default header
